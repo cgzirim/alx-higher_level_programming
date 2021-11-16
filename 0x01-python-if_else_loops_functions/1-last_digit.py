d4688ec95adbeba64a05c8da96eda29dbf3c73d4
Chigozirim Igweamaka <iamokwukwe@gmail.com>
2021-11-16 09:30:26 -0800

    Last digit should be -ve if number is +ve

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,7 +3,12 @@ import random
 number = random.randint(-10000, 10000)
 last_digit = number % 10
 
+#Last digit should be negative if number is negative
+if number < 0:
+    last_digit = -last_digit
+
 print("Last digit of {} is {} ".format(number, last_digit), end="")
+
 if last_digit == 0:
     print("and is 0")
 elif last_digit > 5:
