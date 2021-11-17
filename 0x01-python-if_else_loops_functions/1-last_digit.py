6130d2252b144a117dacb24d81cd120698e50811
Chigozirim Igweamaka <iamokwukwe@gmail.com>
2021-11-16 17:52:26 -0800

    Print the last digit of a number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_digit = number % 10
+last_digit = abs(number) % 10
 
 # Last digit should be negative if number is negative
 if number < 0:
