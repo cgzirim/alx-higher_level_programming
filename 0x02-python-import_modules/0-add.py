31e80ca25b1b4f2d330a573802b0e3b100c10f14
Chigozirim Igweamaka <iamokwukwe@gmail.com>
2021-11-18 04:49:04 -0800

    Add README, do the first task

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def add(a, b):
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, a+b))
+    return (a + b)
+
+
+if __name__ == "__main__":
+    __import__
+    add(1, 2)
