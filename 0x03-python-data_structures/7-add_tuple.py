a0106e2e4d1b896178dc2c2bb5201e25551275b8
Chigozirim Igweamaka <iamokwukwe@gmail.com>
2021-11-21 12:39:58 -0800

    Attempt to please the Checker

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -3,7 +3,6 @@
 
 def add_tuple(tuple_a=(), tuple_b=()):
     """Adds two tuples."""
-
     len_a = len(tuple_a)
     len_b = len(tuple_b)
     # If both tuples are empty assign (0,0) to each.
@@ -14,18 +13,12 @@ def add_tuple(tuple_a=(), tuple_b=()):
 
     # If either tuples have 1 arg, assign 0 as the second arg.
     if len_a == 1:
-        a = tuple_a[0]
-        tuple_a = a, 0
+        tuple_a = tuple_a[0], 0
     if len_b == 1:
-        b = tuple_b[0]
-        tuple_b = b, 0
+        tuple_b = tuple_b[0], 0
 
     # Return the sum of tuples in the format (a,b)
     a0, a1 = tuple_a
     b0, b1 = tuple_b
 
-    x = a0 + b0
-    y = a1 + b1
-    t = x, y
-
-    return (t)
+    return (a0 + b0, a1 + b1)
