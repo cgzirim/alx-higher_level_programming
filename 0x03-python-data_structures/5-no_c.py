b7b48cf1ae2012a33e56bf03b765382f9432a4b2
Chigozirim Igweamaka <iamokwukwe@gmail.com>
2021-11-21 10:16:28 -0800

    Add a second indent before function defination

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
+
 def no_c(my_string):
-    """Removes all characters c and C from a string."""
+    """Remove all characters 'c' and 'C' from a string."""
 
     new = [char for char in my_string if char != 'c' and char != 'C']
     return "".join(new)
