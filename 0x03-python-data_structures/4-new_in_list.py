7683dfd95699c3b17c31b5feaa38a01fe0146706
Chigozirim Igweamaka <iamokwukwe@gmail.com>
2021-11-21 10:09:22 -0800

    Check if the var my_list is a list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,11 +3,11 @@
 def new_in_list(my_list, idx, element):
     """Replaces an element in a list without modifying the original list"""
 
-    if idx < 0:
-        return my_list
-    elif idx > len(my_list):
-        return my_list
-    else:
-        copy = my_list[:]
-        copy[idx] = element
-        return copy
+    if isinstance(my_list, list):
+        if idx >= 0 and idx < len(my_list):
+            """If my_list is a list and idx isn't out of range then do:"""
+            copy = my_list[:]
+            copy[idx] = element
+            return copy
+
+    return my_list
