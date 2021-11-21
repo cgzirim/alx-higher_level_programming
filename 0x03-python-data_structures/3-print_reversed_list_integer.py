4657491994df4c7272383648d8231947dacd9dd4
Chigozirim Igweamaka <iamokwukwe@gmail.com>
2021-11-21 09:45:27 -0800

    Dont use the sort method

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 
+
 def print_reversed_list_integer(my_list=[]):
-    """Prints all integers of a list, in reverse order."""
+    """Print all integers of a list, in reverse order."""
 
-    my_list.sort(reverse=True)
-
-    for num in my_list:
-        print("{}".format(num))
+    for num in range(len(my_list)):
+        print("{:d}".format(my_list.pop()))
