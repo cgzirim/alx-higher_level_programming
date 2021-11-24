665c1a87a84bca7b9c526ec7d0f53c717caf68d1
Igweamaka Chigozirim <iamokwukwe@gmail.com>
2021-11-24 12:28:09 -0800

    Write a function that returns a key with the biggest integer value.

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def best_score(a_dictionary):
+    """Return a key with the biggest integer value."""
+    if a_dictionary is None:
+        return None
+
+    large = 0
+    for k, v in a_dictionary.items():
+        if v > large:
+            large = v
+    for k, v in a_dictionary.items():
+        if v == large:
+            return k
