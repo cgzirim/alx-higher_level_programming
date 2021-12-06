d1c78f400e132dea33df460d57c10d63181f999e
Chigozirim Igweamaka <iamokwukwe@gmail.com>
2021-12-06 10:07:48 -0800

    Write a function that prints an integer

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+def safe_print_integer(value):
+    """Prints an integer with "{:d}".format().
+    value: value can be any type (integer, string, etc.)
+
+    Returns:
+           True if value is an integer.
+           Otherwise, False.
+    """
+    try:
+        print("{:d}".format(value))
+        return True
+    except Exception:
+        print("", end="")
+        return False
