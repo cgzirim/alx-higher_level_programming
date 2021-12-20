b641d1e00bc52d11ef53cfe4bd1b31ba3c8ecb2c
Chigozirim Igweamaka <iamokwukwe@gmail.com>
2021-12-20 12:36:26 -0800

    Check if an object is exactly an instance of a given class.

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -3,5 +3,16 @@
 
 
 def is_same_class(obj, a_class):
-    """Compares an instance and a class."""
-    return (isinstance(obj, a_class))
+    """Check if an object is exactly an instance of  a given class.
+
+    Args:
+        obj (any): The object to check.
+        a_class (type): The type to match the type of obj to.
+
+    Returns:
+        If object is exactly an instance of class - True
+        Otherwise - False
+    """
+    if type(obj) == a_class:
+        return True
+    return False
