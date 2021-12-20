ed821174fee7511fa55e63ab5ccc959a03a0d29f
Chigozirim Igweamaka <iamokwukwe@gmail.com>
2021-12-20 12:22:29 -0800

    Write a function that compares an instance and class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Defines a function that compares an instance and a class."""
+
+
+def is_same_class(obj, a_class):
+    """Compares an instance and a class."""
+    return (isinstance(obj, a_class))
