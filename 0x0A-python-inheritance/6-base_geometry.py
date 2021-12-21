000d8ffe96c208c92f464d4606916a0edbc4521b
Chigozirim Igweamaka <iamokwukwe@gmail.com>
2021-12-20 16:09:29 -0800

    Add a method in 'area' in the BaseGeometry class

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines a class BaseGeometry."""
+
+
+class BaseGeometry():
+    """Initialize BaseGeometry."""
+    def area(self):
+        """Find area."""
+        raise Exception("area() is not implemented")
