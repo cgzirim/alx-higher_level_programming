87a8b37d2f7d77adde043ed7f1881744701142a4
Chigozirim Igweamaka <iamokwukwe@gmail.com>
2021-12-22 04:26:31 -0800

    Write a class MyInt that inherits from int

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/pythob3
+"""Define a class MyInt that inherits from int."""
+
+
+class MyInt(int):
+    """Inverts == and != operators."""
+
+    def __eq__(self, value):
+        """Override == operator with != behavior."""
+        return self.real != value
+
+    def __ne__(self, value):
+        """Override != operator with == behaviour."""
+        return self.real == value
