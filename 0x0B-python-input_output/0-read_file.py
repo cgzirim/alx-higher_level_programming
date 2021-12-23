9084c64574c1d38be45b59049e9980456b149ae8
Chigozirim Igweamaka <iamokwukwe@gmail.com>
2021-12-23 11:47:43 -0800

    Correct error.

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
-"""Defines a function that eads a file."""
+"""Defines a function that reads a file."""
 
 
 def read_file(filename=""):
-    """Reads a text file and prints it to stdout.
-
-    Args:
-        filename - File to be read.
-    """
-    with open(filename, 'r', encoding="utf-8") as f:
+    """Prints the contents of a UTF-8 text file to stdout"""
+    with open(filename, encoding="utf-8") as f:
         print(f.read())
