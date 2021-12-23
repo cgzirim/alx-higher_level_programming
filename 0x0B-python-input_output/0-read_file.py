6937ed65cea565f5177a8cf66477672875eb41e3
Chigozirim Igweamaka <iamokwukwe@gmail.com>
2021-12-23 10:45:42 -0800

    Write a function that reads a text file (UTF8) and prints it to stdout.

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Defines a function that eads a file."""
+
+
+def read_file(filename=""):
+    """Reads a text file and prints it to stdout.
+
+    Args:
+        filename - File to be read.
+    """
+    with open(filename, 'r') as f:
+        print(f.read())
