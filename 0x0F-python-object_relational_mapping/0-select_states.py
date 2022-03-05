f169d6e1c6332e8a25d2b4e3d56e158f8397d7af
Chigozirim Igweamaka <iamokwukwe@gmail.com>
2022-03-05 13:11:13 +0000

    Initial commit

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Lists all states from the database hbtn_0e_0_usa."""
+from sys import argv
+import MySQLdb
+
+
+db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+passwd=argv[2], db=argv[3])
+cur = db.cursor()
+cur.execute("SELECT * FROM states ORDER BY id ASC")
+query_rows = cur.fetchall()
+for row in query_rows:
+	print(row)
+cur.close()
+db.close()
