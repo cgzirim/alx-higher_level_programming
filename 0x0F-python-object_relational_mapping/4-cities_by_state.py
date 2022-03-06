b1d2d2a1b2e973693b0a395fc0b8de00eecff25e
Chigozirim Igweamaka <iamokwukwe@gmail.com>
2022-03-06 13:52:57 +0000

    Correct error and align to pycodestyle.

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -7,7 +7,10 @@ from sys import argv
 
 
 if __name__ == "__main__":
-	db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
-	cur = db.cursor()
-	cur.execute("SELECT * FROM cities ORDER BY id ASC")
-	[print(city) for city in cur.fetchall()]
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    with db.cursor() as cur:
+        cur.execute("SELECT cities.id, cities.name, states.name \
+            FROM cities LEFT JOIN states \
+            ON states.id = cities.state_id \
+            ORDER BY id ASC")
+        [print(city) for city in cur.fetchall()]
