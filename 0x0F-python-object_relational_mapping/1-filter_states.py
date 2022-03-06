1d6e0a09338e81944010345b0c83ffa9ba07a12e
Chigozirim Igweamaka <iamokwukwe@gmail.com>
2022-03-06 12:46:14 +0000

    Close cursor and database after use.

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,8 +10,8 @@ import MySQLdb
 if __name__ == "__main__":
 	db = MySQLdb.connect(host="localhost", port= 3306, user=argv[1],
 		passwd=argv[2], db=argv[3])
-	cur = db.cursor()
-	cur.execute("SELECT * FROM states \
-		WHERE name LIKE 'N%' \
-		ORDER BY id ASC")
-	[print(row) for row in cur.fetchall()]
+	with db.cursor() as cur:
+		cur.execute("SELECT * FROM states \
+			WHERE name LIKE 'N%' \
+			ORDER BY id ASC")
+		[print(row) for row in cur.fetchall()]
