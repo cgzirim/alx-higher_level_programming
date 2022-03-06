779099a34683fbc9ef1f4064fd8ed955c79ab707
Chigozirim Igweamaka <iamokwukwe@gmail.com>
2022-03-06 13:31:08 +0000

    Allign code to pycodestyle and fix error.

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -8,12 +8,9 @@ import MySQLdb
 
 
 if __name__ == "__main__":
-	db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-	passwd=argv[2], db=argv[3])
-	cur = db.cursor()
-	cur.execute("SELECT * FROM states \
-		WHERE name = '{}' \
-		ORDER BY id ASC".format(argv[4]))
-	[print(row) for row in cur.fetchall()]
-	cur.close()
-	db.close()
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    with db.cursor() as cur:
+        cur.execute("SELECT * FROM states \
+            WHERE BINARY name = '{}' \
+            ORDER BY id ASC".format(argv[4]))
+        [print(row) for row in cur.fetchall()]
