a457261bf138cbb0df2038a40ccef4486292ce84
root <iamokwukwe@gmail.com>
2022-03-20 15:11:09 -0700

    Correct error.

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -6,5 +6,7 @@ import requests
 
 if __name__ == "__main__":
     response = requests.get(sys.argv[1])
-    print("Error code: {}".format(response.raise_for_status())
-    print(response.text)
+    if response.status_code >= 400:
+        print("Error code: {}".format(response.status_code))
+    else:
+        print(response.text)
