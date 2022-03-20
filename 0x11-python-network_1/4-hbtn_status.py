89613cadc2990f8d22a49cf376f1f6a5c2b91a50
Igweamaka Chigozirim <iamokwukwe@gmail.com>
2022-03-20 22:18:29 +0100

    Correct error

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Fetches the URL https://alx-intranet.hbtn.io/status."""
+"""Fetches the URL https://intranet.hbtn.io/status."""
 import requests
 
 
@@ -7,4 +7,4 @@ if __name__ == "__main__":
     r = requests.get("https://intranet.hbtn.io/status")
     print("Body response:")
     print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text)
+    print("\t- content: {}".format(r.text))
