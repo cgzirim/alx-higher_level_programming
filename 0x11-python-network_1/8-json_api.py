5b2e982de4d5e50ac4d4c3d3d8ebe7baac7de258
root <iamokwukwe@gmail.com>
2022-03-22 10:57:14 -0700

    Write a Python script that takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user with the letter as a parameter.

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Sends a POST request to http://0.0.0.0:5000/search_user with a given
+letter as a parameter.
+"""
+import sys
+import requests
+
+if __name__ == "__main__":
+    len(sys.argv) < 2 ? value = "" : value = sys.argv[1]
+
+    r = requests.post("http://0.0.0.0:5000/search_user", data={'q':value})
+    try:
+        if len(r.json()) == 0:
+            print('No result')
+        else:
+            d = r.json()
+            print("[{}] {}".format(d.get('id'), d.get('name')))
+    except ValueError:
+        print("Not a valid JSON")
