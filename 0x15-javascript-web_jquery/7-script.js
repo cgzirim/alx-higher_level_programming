ae1ce8fe835031ee14893a427b401122822b3a88
iChigozirim <iamokwukwe@gmail.com>
2022-04-21 00:10:04 +0100

    Correct typo

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,5 +1,5 @@
 $(function () {
-    const url = 'https://swapi-api.hbtn.io/api/people/5/?format=json"
+    const url = 'https://swapi-api.hbtn.io/api/people/5/?format=json'
     $.get(url, function (data, textStatus) {
         if (textStatus === 'success') {
             $('#character').text(data.name)
