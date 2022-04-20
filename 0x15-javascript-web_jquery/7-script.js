e048063aaa5cd99ff5e7b3422a0c0af975b6b1b6
iChigozirim <iamokwukwe@gmail.com>
2022-04-21 00:08:07 +0100

    Write a JavaScript script that fetches the character name from this URL: https://swapi-api.hbtn.io/api/people/5/?format=json

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,8 @@
+$(function () {
+    const url = 'https://swapi-api.hbtn.io/api/people/5/?format=json"
+    $.get(url, function (data, textStatus) {
+        if (textStatus === 'success') {
+            $('#character').text(data.name)
+        }
+    });
+});
